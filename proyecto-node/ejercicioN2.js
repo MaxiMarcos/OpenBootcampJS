6a30eaad615dcd6c964e3350156fea2aa81800b1
Maxi <maximilianoabelmarcos@gmail.com>
2023-01-31 17:29:55 -0300

    ejercicio 2: sintaxis

---
+++ proyecto-node/ejercicioN2.js
@@ -0,0 +1,15 @@
+lista = [
+    
+    "Maxi",
+    26,
+    true, 
+    new Date(1997, 0, 4), 
+    {
+        título: "It", 
+        autor: "Stephen King",
+        fecha: new Date (1986, 8, 15),
+        url: "https://www.cuspide.com/9789877253191/It"
+    }
+]
+
+console.log(lista);
