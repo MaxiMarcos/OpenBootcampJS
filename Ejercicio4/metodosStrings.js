a807387ec51551934ff26928e98c4c48ddebdbbc
Maxi <maximilianoabelmarcos@gmail.com>
2023-02-02 17:13:52 -0300

    ejercicio de strings, métodos

---
+++ Ejercicio4/metodosStrings.js
@@ -0,0 +1,18 @@
+let nombre = "Maxi ";
+
+let apellido = "Marcos";
+
+let estudiante = nombre.concat(apellido);
+
+let mayusEst = estudiante.toUpperCase();
+let minusEst = estudiante.toLowerCase();
+
+let numLetras = estudiante.length;
+
+let letraNombre = nombre[0];
+
+let finalApellido = apellido.slice(-1);
+
+let sinEspacio = estudiante.replace(/ /g, "");
+
+let estudianteBool = estudiante.includes(nombre);
