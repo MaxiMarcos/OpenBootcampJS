5ce6b57b158fe0f661eb4fc0c35102c7642b7167
Maxi <maximilianoabelmarcos@gmail.com>
2023-02-03 11:47:37 -0300

    ejercicio métodos de num

---
+++ Ejercicio5/ejercicioNumeros.js
@@ -0,0 +1,11 @@
+let altura_cm = 179;
+let altura_m = 1.79;
+let peso = 74.5;
+
+
+let abajo = Math.floor(altura_m);
+let arriba = Math.ceil(peso);
+
+let compara = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+console.log(arriba);
